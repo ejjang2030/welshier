5a65490f954509783f1f40872177d5ca57adbbbc
ejjang2030 <ejjang2030@gmail.com>
2024-10-01 01:00:49 +0900

    fix : settings 화면 styling

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,12 +1,16 @@
 import {languageState} from "atom";
 import {getAuth, signOut} from "firebase/auth";
 import {app} from "firebaseApp";
+import {AiOutlineArrowLeft as GoBackIcon} from "react-icons/ai";
+import {FaLanguage as ChangeLanguageIcon} from "react-icons/fa6";
 import {initializeI18n} from "i18n";
 import {useTranslation} from "react-i18next";
 import {useRecoilState} from "recoil";
 import {changeDayJsLang} from "utils/TimeUtils";
+import {useNavigate} from "react-router-dom";
 
 const SettingsPage = () => {
+  const navigate = useNavigate();
   const {t, i18n} = useTranslation();
   const [currLang, setCurrLang] = useRecoilState(languageState);
 
@@ -28,18 +32,31 @@ const SettingsPage = () => {
   };
 
   return (
-    <>
-      <button
-        type='button'
-        onClick={handleLogout}>
-        로그아웃
-      </button>
-      <button
-        type='button'
-        onClick={handleChangeLanguage}>
-        한영변환
-      </button>
-    </>
+    <div>
+      <div className='post-new__appbar'>
+        <div className='post-new__appbar-box-left'>
+          <GoBackIcon
+            className='post-new__appbar-left'
+            onClick={() => navigate(-1)}
+          />
+          <span>설정</span>
+        </div>
+      </div>
+      <div className='settings__body'>
+        <div
+          className='settings__body-item'
+          onClick={handleChangeLanguage}>
+          <ChangeLanguageIcon className='icon' />
+          <span>언어 :</span>
+          <span>{currLang === "ko" ? "한국어" : "English"}</span>
+        </div>
+        <div
+          className='settings__body-item logout'
+          onClick={handleLogout}>
+          로그아웃
+        </div>
+      </div>
+    </div>
   );
 };
 
